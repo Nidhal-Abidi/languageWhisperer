70fd0c9f9c1d936d6d1d3a9766cf5d2a13fb6714
Nidhal-Abidi <nidhalabidi201@gmail.com>
2025-05-04 22:37:54 +0200

    fix: remove the FormFields component

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import { FormFields } from "./FormFields";
+import { FormContainer } from "./FormContainer";
 import { FormHeader } from "./FormHeader";
 
 export const Form = () => {
@@ -6,14 +6,7 @@ export const Form = () => {
     <div className="relative flex size-full min-h-screen flex-col bg-white group/design-root overflow-x-hidden">
       <div className="layout-container flex h-full grow flex-col">
         <FormHeader />
-        <form className="px-40 flex flex-1 justify-center py-5">
-          <div className="flex flex-col w-[512px] py-5 max-w-[960px] flex-1">
-            <h2 className="text-[#111418] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5">
-              Fill the form
-            </h2>
-            <FormFields />
-          </div>
-        </form>
+        <FormContainer />
       </div>
     </div>
   );
---
+++ frontend/src/components/form/FormContainer.tsx
@@ -0,0 +1,27 @@
+import { ConversationScenarios } from "./ConversationScenarios";
+import { LanguageGrid } from "./LanguageGrid";
+import { LanguageProficiency } from "./LanguageProficiency";
+
+export const FormContainer = () => {
+  return (
+    <form className="px-40 flex flex-1 justify-center py-5">
+      <div className="flex flex-col w-[512px] py-5 max-w-[960px] flex-1">
+        <h2 className="text-[#111418] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5">
+          Fill the form
+        </h2>
+        <LanguageProficiency />
+        <LanguageGrid type="conversation-language" />
+        <LanguageGrid type="translation-language" />
+        <ConversationScenarios />
+        <div className="flex px-4 py-3 justify-center">
+          <button
+            type="submit"
+            className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#5583B4] text-white text-sm font-bold leading-normal tracking-[0.015em] transition-all duration-300 ease-in-out hover:bg-[#304D6D]"
+          >
+            Start Practicing
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
