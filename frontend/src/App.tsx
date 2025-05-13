e2be2594a12d9693d5ed57274f3b24376af7ade3
Nidhal-Abidi <nidhalabidi201@gmail.com>
2025-05-13 19:24:36 +0200

    add default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import "./App.css";
 import { Form } from "./components/form/Form";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { Session } from "./components/session/Session";
 
 function App() {
@@ -10,8 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/sessions/:sessionId" element={<Session />} />
-        {/* Instead of a 404 Error page, move to the Form page + remove the wrong pathname */}
-        <Route path="*" element={<Form />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
